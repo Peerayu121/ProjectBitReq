530a90b9006f0ea7e6c9f5620be80f4620a605a0
Peerayu121 <sampeerayu@gmail.com>
2023-09-29 20:41:11 +0700

    css(external)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,77 @@
-<x-guest-layout>
-    <div class="nav2">
-            <style>
-                .nav2{
-                    background-color: #93B1C7;
-                    /* border: 1px solid; */
-                    height: 100px;
-                    border-radius: 4%;
-                }
-            </style>
-    </div>
-    <x-authentication-card>
+<!DOCTYPE html>
+<html lang="en">
 
-        {{-- <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot> --}}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('./css/styles.css')}}">
+</head>
 
-        <x-validation-errors class="mb-4" />
+<body>
+    <x-guest-layout>
+        <div class="nav2">
+        </div>
+        <x-authentication-card>
+            <div class="container">
+                <x-validation-errors class="mb-4" />
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <center>
+                    <p>เข้าสู่ระบบ</p>
+                </center>
+                <br>
+                <center>
+                    <p>กรุณากรอกอีเมลและรหัสผ่าน</p>
+                </center>
+
+                <div>
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        required autofocus autocomplete="username" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="current-password" />
+                </div>
+
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-checkbox id="remember_me" name="remember" />
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+
+                    <x-button class="ml-4">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
+            </form>
             </div>
-        @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <center><p>เข้าสู่ระบบ</p></center>
-            <br>
-            <center><p>กรุณากรอกอีเมลและรหัสผ่าน</p></center>
+            
+        </x-authentication-card>
+    </x-guest-layout>
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+</body>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+</html>
